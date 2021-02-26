6cda5f1ad001683dcf6cd4ca7f25ce83fad9a579
Brian Austin <brianjaustin@gmail.com>
2021-02-26 10:28:24 -0500

    Fix build/start port

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4792
+export PORT=4794
 
 echo "Stopping old copy of app, if any..."
 
